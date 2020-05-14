18127c0842236dece92fe0ef2b92b75768a3d757
michellescribner <56173868+michellescribner@users.noreply.github.com>
2020-05-14 11:05:20 -0400

    Add files via upload

--- paeruginosa_WGS/mutation_filtering.R
+++ paeruginosa_WGS/mutation_filtering.R
@@ -4,9 +4,7 @@ library("reshape2")
 
 setwd("/Users/mrs/Documents/tob_paper_data/paeruginosa_WGS")
 
-# Mutations called in breseq as described in Scribner et al were converted to CSV files with no utf characters 
-# sample names assigned for sequencing were already replaced with more informative sample names
-
+# Mutations called in breseq as described in Scribner et al were converted to CSV files with no utf characters and informative sample names were substituted for those assigned for sequencing.
 snps <- read.csv("/Users/mrs/Documents/tob_paper_data/paeruginosa_WGS/pa_tob_snps.csv",header=TRUE)
 
 # Remove SNPs already present in the ancestor
@@ -18,12 +16,14 @@ nrow(snps_noref)#1429
 snps_noref$gene_annot <- paste(snps_noref$Gene,snps_noref$Annotation,sep="::")
 
 ## remove manually filtered mutations
-# the breseq documentation as of 4/30/2020 reads "Polymorphism prediction is still considered a somewhat experimental feature. It continues to be actively developed." Therefore, we took care to throughly examine mutations reported by breseq in polymorphism mode. Mutations that appear biologically implausible were removed from further analysis. These mutations and the rationale motivating their removal are listed in manuallyfiltered_mutations.
+# The breseq documentation as of 4/30/2020 reads "Polymorphism prediction is still considered a somewhat experimental feature. It continues to be actively developed." 
+# Therefore, we manually examined read pileups for mutations reported by breseq in polymorphism mode. Mutations that appear biologically implausible were removed from further analysis. 
+# These mutations and the rationale motivating their removal are listed in manuallyfiltered_mutations and in the manuscript.
 manuallyfiltered_mutations <- read.csv("/Users/mrs/Documents/tob_paper_data/paeruginosa_WGS/manuallyfiltered_mutations.csv", header=TRUE)
 manuallyfiltered_mutations$gene_annot <- paste(manuallyfiltered_mutations$gene,manuallyfiltered_mutations$annotation,sep="::")
 snps_noref <- snps_noref[ !(snps_noref$gene_annot %in% manuallyfiltered_mutations$gene_annot), ]
 
-### Rename genes of interest to gene names or old ref seq names
+### Rename locus tages to gene names or old RefSeq locus tags
 snps_noref_clean <- snps_noref
 snps_noref_clean$Gene <- gsub( "fusA", "fusA1", as.character(snps_noref_clean$Gene))
 snps_noref_clean$Gene <- gsub( "PA14_RS01795", "ptsP", as.character(snps_noref_clean$Gene))
@@ -57,6 +57,7 @@ sample$pop <- gsub( "Pop ", "", as.character(sample$pop))
 snps_noref_clean <- cbind(sample, snps_noref_clean)
 
 snps <- snps_noref_clean
+
 #combine gene and annotation in one column
 snps$gene_annot_desc <-  paste(snps$Gene,snps$Annotation, snps$Description,sep="::")
 snps$gene_annot_desc <- as.factor(snps$gene_annot_desc)
@@ -73,7 +74,7 @@ colnames(snps_cast) <- as.character(unlist(snps_cast[1,]))
 #remove sample column
 snps_cast <- snps_cast[-1,]
 #convert frequency values to numeric class - start as "character"
-snps_cast[,1:ncol(snps_cast)] <-apply(snps_cast[,1:ncol(snps_cast)], 2, function(x) as.numeric(as.character(x)))
+snps_cast[,1:ncol(snps_cast)] <- apply(snps_cast[,1:ncol(snps_cast)], 2, function(x) as.numeric(as.character(x)))
 nrow(snps_cast) #1009
 snps_cast$Sums <- rowSums(snps_cast)
 
@@ -88,22 +89,8 @@ write.csv(t_snps_cast_25, file="/Users/mrs/Documents/tob_paper_data/paeruginosa_
 
 # read in NJE mutations
 nje <- read.csv("/Users/mrs/Documents/tob_paper_data/paeruginosa_WGS/pa_tob_nje.csv",header=TRUE)
-#nje splits mutations into two rows (one row for each side of the junction), must combine into one row for further analysis
-evens <- seq(from = 2, to = nrow(nje), by = 2)
-nje_even <- nje[evens, ]
-cols_even <- paste("2", colnames(nje_even), sep = ".")
-colnames(nje_even) <- cols_even
-nje_odd <- nje[-evens, ]
-nje_done <- cbind(nje_odd, nje_even)
 
-#substitute sample names
-samplekey <- read.csv("/Users/mrs/Documents/PA14_Tobi_M9/breseq/samplekey_pa14tobm9.csv",header=TRUE)
-nje <- (merge(nje_done,samplekey,by="Sample"))
-
-#remove populations from a different experiment
-nje <- subset(nje, nje$Model != "TIP")
-
-#remove nje mutations that were in the ancestor sequence
+#remove nje mutations that were in the ancestor's breseq output
 nje$gene_annot_desc <- paste(nje$Gene,nje$Annotation, nje$Product, nje$`2.Gene`, nje$`2.Annotation`, nje$`2.Product`,sep="::")
 nje_noref <- subset(nje, nje$gene_annot_desc != "PA14_RS30510/PA14_RS14515::intergenic(146/+106)::hypothetical protein/hypothetical protein::PA14_RS14525/PA14_RS14530::intergenic(69/218)::hypothetical protein/Tn3 family transposase")
 nje_noref <- subset(nje_noref, nje_noref$gene_annot_desc != "PA14_RS19845/PA14_RS19850::intergenic(45/+61)::tRNA 2thiocytidine(32) synthetase TtcA/integrase::PA14_RS31160/PA14_RS19915::intergenic(+317/59)::hypothetical protein/hypothetical protein")
@@ -124,13 +111,14 @@ nje_cast[,1:ncol(nje_cast)] <-apply(nje_cast[,1:ncol(nje_cast)], 2, function(x) 
 nrow(nje_cast) #44
 nje_cast$Sums <- rowSums(nje_cast)
 
-# mutations in nje_cast were further examined manually by analyzing read pileups 
+# Mutations in nje_cast were further examined manually by analyzing read pileups.
 
-# NJE evidence is difficult to filter in R because mutation calls represent two rows of information (describing either side of the junction). 
 # We took a conservative approach to analyzing junction calls identified in polymorphism mode: 
-# we included in further analysis only junction calls that occurred in loci (or loci in related pathways) that were confirmed to confer tobramycin resistance 
-# by clonal WGS and phenotyping (ptsP, HPr, and NADH quinone oxidoreductase) or loci previously associated with aminoglycoside resistance (23S rRNA).
+# we included in further analysis only junction calls that occurred in loci (or loci in related pathways) that were confirmed to confer tobramycin resistance by clonal WGS and phenotyping
+# or loci previously associated with aminoglycoside resistance (ptsP, HPr, and NADH quinone oxidoreductase for A. baumannii and 23S rRNA for P. aeruginosa).
 
-# One mutation in 23S rRNA was determined to reach high enough frequencies and occur consistently enough to include in subsequent analysis,
-# whereas other mutations were singletons or reached only low frequencies. 
-# This mutation was included manually in subsequent analysis.
+# While several mutations in 23S rRNA were reported by breseq, only one was determined to be of high enough quality by examining read pileups to be included in subsequent analysis. 
+# This mutation was estimated to occur at high frequencies at consecutive timepoints whereas other 23S rRNA other mutations were singletons or reached only low frequencies (ie, would not pass frequency filters applied to SNPs). 
+# This mutation also occured at trajectories that complemented the allele frequencies of other contending mutations in that population.
+# Thus, this mutation was included in subsequent analysis as its read pileups and dynamics both supported that it was an important player influencing the population dynamics within that population.
+
